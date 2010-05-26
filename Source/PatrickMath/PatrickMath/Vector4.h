7ac04ed25fa9669713af7671c963c6a80cb5bd62
pux1987 <pux1987@2a84f488-4750-3b91-4b16-9a5d11d86cc2>
2010-05-26 01:48:32 +0000

    PatrickMath Updated the project and added some comments

--- Source/PatrickMath/PatrickMath/Vector4.h
+++ Source/PatrickMath/PatrickMath/Vector4.h
@@ -1,3 +1,40 @@
+/*!
+* \file Vector4.h
+* \author Patrick Martin
+* \date 2010
+* \brief A fast vector library written using SSE instruction
+* 
+* This is an affine vector implementation.  It is assumed that all offset vectors will have a fourth coordinate of 0
+* and that all points will have a fourth coordinate or 1.  All operations are performed using SIMD registers, therefore
+* it is necessary to use the provided container class to load data and retrieve results.  Transfer is comparatively
+* slow and therefore should be avoided.
+*
+* This project is governed by the MIT licence:
+* 
+*  Copyright (c) 2010 Patrick Martin
+* 
+*  Permission is hereby granted, free of charge, to any person
+*  obtaining a copy of this software and associated documentation
+*  files (the "Software"), to deal in the Software without
+*  restriction, including without limitation the rights to use,
+*  copy, modify, merge, publish, distribute, sublicense, and/or sell
+*  copies of the Software, and to permit persons to whom the
+*  Software is furnished to do so, subject to the following
+*  conditions:
+* 
+*  The above copyright notice and this permission notice shall be
+*  included in all copies or substantial portions of the Software.
+* 
+*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+*  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+*  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+*  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+*  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+*  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+*  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+*  OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #pragma once
 
 #include <xmmintrin.h>
