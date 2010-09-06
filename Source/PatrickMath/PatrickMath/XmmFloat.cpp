461296b3bbb69a57c8d4d651c8e592d9ff38d830
pux1987@gmail.com <pux1987@gmail.com@2a84f488-4750-3b91-4b16-9a5d11d86cc2>
2010-09-06 15:32:04 +0000

    PATRICKMATH Added a basic (but untested) implementation of cos/sin (out of boredom this morning) and a not working quaternion class (mainly because I keep meaning to work on it while away from my main pc but I don't have the latest revisions).

--- Source/PatrickMath/PatrickMath/XmmFloat.cpp
+++ Source/PatrickMath/PatrickMath/XmmFloat.cpp
@@ -2,6 +2,18 @@
 
 #include "XmmFloat.h"
 #include <limits>
+#include <stdint.h>
 
 const XmmFloat XmmFloat::EPSILON = XmmFloat(std::numeric_limits<float>::epsilon());
 const XmmFloat XmmFloat::EPSILON_SQ = XmmFloat::EPSILON * XmmFloat::EPSILON;
+
+const XmmFloat XmmFloat::_PI = XmmFloat(3.14159265f);
+const XmmFloat XmmFloat::_PI_2 = XmmFloat::_PI / XmmFloat(2.f);
+const XmmFloat XmmFloat::_2PI = XmmFloat::_PI * XmmFloat(2.f);
+
+union intfloat
+{
+	int32_t intPart;
+	float floatPart;
+} const floatMask = { 0x7FFFFFFF };
+const XmmFloat XmmFloat::_FLOAT_ABS_MASK = XmmFloat(floatMask.floatPart);
